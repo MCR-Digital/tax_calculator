8bcc998757482e10a927d6e8454e5499deef3d28
Jacky Price <priceja@C02VC3QNG8WN.local>
2019-01-21 11:41:08 +0000

    neater code

--- exercises/java/src/main/java/tax/SimpleTaxCalculator.java
+++ exercises/java/src/main/java/tax/SimpleTaxCalculator.java
@@ -5,44 +5,33 @@ public class SimpleTaxCalculator extends TaxCalculator {
     @Override
     int calculateTax(Vehicle vehicle) {
         int emissions = vehicle.getCo2Emissions();
-        
-        if(emissions==0){
+
+        if (emissions == 0) {
             return 0;
         }
-        if(emissions<=50 && emissions>0){
+        if (emissions <= 50 && emissions > 0) {
             return 10;
-        }
-        else if(emissions<=75 && emissions>50){
+        } else if (emissions > 50 && emissions <= 75) {
             return 25;
-        }
-        else if(emissions<=90 && emissions>75){
+        } else if (emissions > 75 && emissions <= 90) {
             return 105;
-        }
-        else if(emissions<=100 && emissions>90){
+        } else if (emissions > 90 && emissions <= 100) {
             return 125;
-        }
-        else if(emissions<=110 && emissions>100){
+        } else if (emissions > 100 && emissions <= 110) {
             return 145;
-        }
-        else if(emissions<=130 && emissions>110){
+        } else if (emissions > 110 && emissions <= 130) {
             return 165;
-        }
-        else if(emissions<=150 && emissions>130){
+        } else if (emissions > 130 && emissions <= 150) {
             return 205;
-        }
-        else if(emissions<=170 && emissions>150){
+        } else if (emissions > 150 && emissions <= 170) {
             return 515;
-        }
-        else if(emissions<=190 && emissions>170){
+        } else if (emissions > 170 && emissions <= 190) {
             return 830;
-        }
-        else if(emissions<=225 && emissions>190){
+        } else if (emissions > 190 && emissions <= 225) {
             return 1240;
-        }
-        else if(emissions<=255 && emissions>225){
+        } else if (emissions <= 255 && emissions > 225) {
             return 1760;
-        }
-        else if(emissions>255){
+        } else if (emissions > 255) {
             return 2070;
         }
 
