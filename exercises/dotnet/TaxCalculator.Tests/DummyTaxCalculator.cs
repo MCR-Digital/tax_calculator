9ac0b540aa1562d5d012b4aa923abf3896c47bbd
ukales <audrey_lescot@waters.com>
2021-05-17 11:21:49 +0100

    Added run tests for petrol only ignored for the rest

--- exercises/dotnet/TaxCalculator.Tests/DummyTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DummyTaxCalculator.cs
@@ -2,6 +2,7 @@
 
 namespace TaxCalculator.Tests
 {
+    
     [Obsolete("DO NOT MAKE ANY CHANGES TO THIS CLASS - IT IS JUST HERE TO KEEP THE TESTS HAPPY TO BEGIN WITH. DON'T BE LAZY - WRITE YOUR OWN NEW TAXCALCULATOR CLASS")]
     public sealed class DummyTaxCalculator : TaxCalculator
     {
