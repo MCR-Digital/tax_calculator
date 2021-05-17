c64b003b332a064cf9e87755a83b431657269f09
ukales <audrey_lescot@waters.com>
2021-05-17 13:32:14 +0100

    equals

--- exercises/dotnet/TaxCalculator/CalculateTax.cs
+++ exercises/dotnet/TaxCalculator/CalculateTax.cs
@@ -13,43 +13,43 @@ namespace TaxCalculator
             {
                 return 0;
             }
-            else if(vehicle.Co2Emissions < 51)
+            else if(vehicle.Co2Emissions <= 50)
             {
                 return 10;
             }
-            else if (vehicle.Co2Emissions < 76)
+            else if (vehicle.Co2Emissions <= 75)
             {
                 return 25;
             }
-            else if (vehicle.Co2Emissions < 91)
+            else if (vehicle.Co2Emissions <= 90)
             {
                 return 105;
             }
-            else if (vehicle.Co2Emissions < 101)
+            else if (vehicle.Co2Emissions <= 100)
             {
                 return 125;
             }
-            else if (vehicle.Co2Emissions < 111)
+            else if (vehicle.Co2Emissions <= 110)
             {
                 return 145;
             }
-            else if (vehicle.Co2Emissions < 131)
+            else if (vehicle.Co2Emissions <= 130)
             {
                 return 165;
             }
-            else if (vehicle.Co2Emissions < 151)
+            else if (vehicle.Co2Emissions <= 150)
             {
                 return 205;
             }
-            else if (vehicle.Co2Emissions < 171)
+            else if (vehicle.Co2Emissions <= 170)
             {
                 return 515;
             }
-            else if (vehicle.Co2Emissions < 191)
+            else if (vehicle.Co2Emissions <= 190)
             {
                 return 830;
             }
-            else if (vehicle.Co2Emissions < 226)
+            else if (vehicle.Co2Emissions <= 225)
             {
                 return 1240;
             }
